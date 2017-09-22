e411b8687b12a9bac6b202dc062fc0a60838af28
lukasz.modlinski <lukasz.modlinski@gmail.com>
2017-09-23 01:43:03 +0200

    all files, no real ignore file

---
+++ android/src/com/psychogra/slon2/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.psychogra.slon2;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.psychogra.slon2.SlonMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SlonMain(), config);
+	}
+}
---
+++ desktop/src/com/psychogra/slon2/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.psychogra.slon2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.psychogra.slon2.SlonMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SlonMain(), config);
+	}
+}
