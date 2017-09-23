e61067fb1df43a684db410d1e860264d3e804de5
lukasz.modlinski <lukasz.modlinski@gmail.com>
2017-09-23 22:08:34 +0200

    enum for rules

--- core/src/com/psychogra/slon2/models/rules/Rule.java
+++ core/src/com/psychogra/slon2/models/rules/Rule.java
@@ -11,4 +11,21 @@ abstract public class Rule
 	public abstract boolean isValid(List model, List progress);
 
 	public abstract boolean isFulfilled(List model, List progress);
+
+	enum Type
+	{
+		EQUALITY("equality"), SEQUENCE("sequence");
+
+		private String type;
+
+		private Type(String type)
+		{
+			this.type = type;
+		}
+
+		public String getType()
+		{
+			return type;
+		}
+	}
 }
