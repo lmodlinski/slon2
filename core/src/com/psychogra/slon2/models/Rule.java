991f49bef5134d742ab9a33480ea5261bd4326f4
lukasz.modlinski <lukasz.modlinski@gmail.com>
2017-09-23 14:08:28 +0200

    rules + seq rule

--- core/src/com/psychogra/slon2/models/Rule.java
+++ core/src/com/psychogra/slon2/models/Rule.java
@@ -1,9 +1,12 @@
 package com.psychogra.slon2.models;
 
+import java.util.List;
+
 /**
  * Created by lmodlinski on 23/09/2017.
  */
 
 abstract public class Rule
 {
+	public abstract boolean isFullfilled(List<GameObject> model, List<GameObject> progress);
 }
