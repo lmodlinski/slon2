1fb0cedbef746056a3071bd182cbe9dd320067a9
mszym <marcel.szymanski@tensquaregames.com>
2017-09-23 18:49:30 +0200

    działa za pierwszym razem

---
+++ core/src/com/psychogra/slon2/config.java
@@ -0,0 +1,15 @@
+package com.psychogra.slon2;
+
+/**
+ * Created by Marcel on 2017-09-23.
+ */
+
+public class config {
+    private static float width;
+    private static float height;
+
+    public static void setRatio(float newWidth,float newHeight){
+        width  = newWidth;
+        height = newHeight;
+    }
+}
